f53840602af18a02ee1c3cacc2a06db5d4d9bfd8
Paweł Pankiewicz <pawelp6020@gmail.com>
2021-05-12 22:36:48 +0200

    Stan na 22:36

--- worker.js
+++ worker.js
@@ -16,12 +16,9 @@ const client = new Client(config);
 client.subscribe(
   "remove_item_from_inventory",
   async ({ task, taskService }) => {
+    const i = task.variables.get("loopCounter");
     console.log(leki);
     console.log(task.variables.getAll());
-    const var1 = new Variables().set(
-      "test1",
-      "HELLO AND AGAIN WELCOME TO APERTURE SCIENCE COMPUTER AIDED ENRICHMENT CENTRE!"
-    );
     await taskService.complete(task, var1);
   }
 );
